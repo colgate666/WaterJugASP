51b14f0cabfc01d30c806a1722133958210500d3
colgate666 <jesusmolinacz@gmail.com>
2023-07-07 09:48:14 -0400

    stack overflow fix

--- WaterJugASP/Utils/WaterJugSolver.cs
+++ WaterJugASP/Utils/WaterJugSolver.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using WaterJugASP.Models;
+﻿using WaterJugASP.Models;
 
 namespace WaterJugASP.Utils
 {
@@ -41,27 +40,31 @@ namespace WaterJugASP.Utils
         /// <returns>The solution if it exists, null otherwise.</returns>
         public static List<WaterJugModel>? FindSolution(WaterJugModel input)
         {
-            try
-            {
-                List<WaterJugModel> xSteps = new(), ySteps = new();
+            List<WaterJugModel> xSteps = new(), ySteps = new();
 
-                //Run the solve function for both x and y as the main jug
-                Solve(0, 0, input, MainJug.X, xSteps);
-                Solve(0, 0, input, MainJug.Y, ySteps);
+            //Run the solve function for both x and y as the main jug
+            var solFoundX = Solve(0, 0, input, MainJug.X, xSteps, new());
+            var solFoundY = Solve(0, 0, input, MainJug.Y, ySteps, new());
 
-                //If x is the shortest, return it
-                if (xSteps.Count < ySteps.Count)
-                {
-                    _steps = xSteps;
-                    return _steps;
-                }
-
-                _steps = ySteps;
-                return _steps;
-            }
-            catch (Exception)
+            if (!solFoundX && !solFoundY)
             {
                 return null;
+            } else if (solFoundX && !solFoundY)
+            {
+                _steps = xSteps;
+                return _steps;
+            } else if (!solFoundX && solFoundY)
+            {
+                _steps = ySteps;
+                return _steps;
+            } else if (xSteps.Count < ySteps.Count)
+            {
+                _steps = xSteps;
+                return _steps;
+            } else
+            {
+                _steps = ySteps;
+                return _steps;
             }
         }
 
@@ -73,54 +76,61 @@ namespace WaterJugASP.Utils
         /// <param name="input">The values to use in the Water Jug problem</param>
         /// <param name="mainJug">The main jug to use for the next iteration</param>
         /// <param name="steps">The steps that have been taken so far</param>
-        private static void Solve(int x, int y, WaterJugModel input, MainJug mainJug, List<WaterJugModel> steps)
+        private static bool Solve(int x, int y, WaterJugModel input, MainJug mainJug, List<WaterJugModel> steps, HashSet<KeyValuePair<int, int>> visited)
         {
-            steps.Add(new() { X = x, Y = y, Z = input.Z }); //Add the current state to the steps array
-
             //If we have reached the goal, return
             if (x == input.Z || y == input.Z)
             {
-                return;
+                steps.Add(new() { X = x, Y = y, Z = input.Z }); //Add the current state to the steps array
+                return true;
             }
 
-            //If the main jug is x, then we need to try to fill it up first
-            if (mainJug == MainJug.X)
+            if (!visited.Contains(new(x, y)))
             {
-                //Empty y if it's full
-                if (y == input.Y)
+                steps.Add(new() { X = x, Y = y, Z = input.Z });
+                visited.Add(new(x, y));
+
+                //If the main jug is x, then we need to try to fill it up first
+                if (mainJug == MainJug.X)
                 {
-                    Solve(x, 0, input, mainJug, steps);
-                }
-                else if (x == 0)
-                { //Fill x if it's empty
-                    Solve(input.X, y, input, mainJug, steps);
+                    //Empty y if it's full
+                    if (y == input.Y)
+                    {
+                        return Solve(x, 0, input, mainJug, steps, visited);
+                    }
+                    else if (x == 0)
+                    { //Fill x if it's empty
+                        return Solve(input.X, y, input, mainJug, steps, visited);
+                    }
+                    else
+                    { //Transfer water from x to y
+                        int remSpace = input.Y - y;
+                        int pouredWater = x > remSpace ? remSpace : x;
+
+                        return Solve(x - pouredWater, y + pouredWater, input, mainJug, steps, visited);
+                    }
                 }
                 else
-                { //Transfer water from x to y
-                    int remSpace = input.Y - y;
-                    int pouredWater = x > remSpace ? remSpace : x;
+                { //If the main jug is y, then we need to try to fill it up first
+                    if (x == input.X)
+                    {
+                        return Solve(0, y, input, mainJug, steps, visited);
+                    }
+                    else if (y == 0)
+                    {
+                        return Solve(x, input.Y, input, mainJug, steps, visited);
+                    }
+                    else
+                    {
+                        int remSpace = input.X - x;
+                        int pouredWater = y > remSpace ? remSpace : y;
 
-                    Solve(x - pouredWater, y + pouredWater, input, mainJug, steps);
+                        return Solve(x + pouredWater, y - pouredWater, input, mainJug, steps, visited);
+                    }
                 }
             }
-            else
-            { //If the main jug is y, then we need to try to fill it up first
-                if (x == input.X)
-                {
-                    Solve(0, y, input, mainJug, steps);
-                }
-                else if (y == 0)
-                {
-                    Solve(x, input.Y, input, mainJug, steps);
-                }
-                else
-                {
-                    int remSpace = input.X - x;
-                    int pouredWater = y > remSpace ? remSpace : y;
 
-                    Solve(x + pouredWater, y - pouredWater, input, mainJug, steps);
-                }
-            }
+            return false;
         }
     }
 }
